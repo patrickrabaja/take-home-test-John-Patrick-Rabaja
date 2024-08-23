537687a0d61bab7443803b62497d183fa10b603e
Patrick Rabaja <61625031+patrickrabaja@users.noreply.github.com>
2024-08-23 11:27:17 +0800

    Update login.cy.ts

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -4,20 +4,30 @@ const loginPage = new LoginPage();
 
 describe("Facebook Auth", () => {
   it("Login with Facebook", () => {
-    cy.visit("/"); //URL
+    cy.visit("/login"); //URL
     cy.get("#loginWithFacebook").click();
     loginPage.loginWithFacebook("username", "password");
-    cy.get("#success").should("be.visible"); //Assert Success Notification
+    cy.get("#success").should("be.visible"); //Assert Assumed Success Notification
   });
 });
 
 describe("Google Auth", () => {
   it("Login with Google", () => {
-    cy.visit("/"); //URL
+    cy.visit("/login"); //URL
     cy.get("#loginWithGoogle").click();
     loginPage.loginWithGoogle("username", "password");
-    cy.get("#success").should("be.visible"); //Assert Success Notification
+    cy.get("#success").should("be.visible"); //Assert Assumed Success Notification
   });
 });
 
-describe("2FA Auth");
+describe("Local Account", () => {
+  it("Login with Local Account", () => {
+    cy.visit("/login"); //URL
+    cy.get("#email").click();
+    cy.get("#email").type("username", { log: false }); //to prevent revealing info in results
+    cy.get("#pass").click();
+    cy.get("#pass").type("pass", { log: false }); //to prevent revealing info in results
+    cy.get("#login").click();
+    cy.get("#success").should("be.visible"); //Assert Assumed Success Notification
+  });
+});
